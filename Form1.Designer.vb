66b85543637366e8c6221fc3723341c47c159191
Rex-2-ui <rexraymond35@gmail.com>
2026-02-03 09:22:13 +0000

    Codes for Reset and Exit Buttons

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,27 +23,27 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         GroupBox1 = New GroupBox()
-        GroupBox2 = New GroupBox()
-        Button1 = New Button()
-        Button2 = New Button()
-        Button3 = New Button()
-        Button4 = New Button()
-        Label1 = New Label()
-        Label2 = New Label()
-        Label3 = New Label()
-        Label4 = New Label()
-        Label5 = New Label()
-        Label6 = New Label()
-        Label7 = New Label()
-        Label8 = New Label()
-        TextBox1 = New TextBox()
         TextBox2 = New TextBox()
+        TextBox1 = New TextBox()
+        Label2 = New Label()
+        Label1 = New Label()
+        GroupBox2 = New GroupBox()
         TextBox3 = New TextBox()
         TextBox4 = New TextBox()
         TextBox5 = New TextBox()
         TextBox6 = New TextBox()
         TextBox7 = New TextBox()
         TextBox8 = New TextBox()
+        Label8 = New Label()
+        Label7 = New Label()
+        Label6 = New Label()
+        Label5 = New Label()
+        Label4 = New Label()
+        Label3 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
         GroupBox1.SuspendLayout()
         GroupBox2.SuspendLayout()
         SuspendLayout()
@@ -62,6 +62,42 @@ Partial Class Form1
         GroupBox1.TabIndex = 0
         GroupBox1.TabStop = False
         GroupBox1.Text = "Part Information"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(240, 65)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(400, 27)
+        TextBox2.TabIndex = 6
+        TextBox2.Text = "Radiator, Ford Escort"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(240, 20)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(400, 27)
+        TextBox1.TabIndex = 2
+        TextBox1.Text = "X456YVK98"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.ForeColor = SystemColors.Desktop
+        Label2.Location = New Point(56, 65)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(119, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Part Description"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.ForeColor = SystemColors.ControlText
+        Label1.Location = New Point(56, 27)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(103, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Part Identifier"
         ' 
         ' GroupBox2
         ' 
@@ -85,142 +121,6 @@ Partial Class Form1
         GroupBox2.TabIndex = 1
         GroupBox2.TabStop = False
         GroupBox2.Text = "Price, Cost, Storage, Profit"
-        ' 
-        ' Button1
-        ' 
-        Button1.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Button1.Location = New Point(699, 207)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(94, 29)
-        Button1.TabIndex = 2
-        Button1.Text = "Compute"
-        Button1.UseVisualStyleBackColor = True
-        ' 
-        ' Button2
-        ' 
-        Button2.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Button2.Location = New Point(699, 265)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(94, 29)
-        Button2.TabIndex = 3
-        Button2.Text = "Reset Form"
-        Button2.UseVisualStyleBackColor = True
-        ' 
-        ' Button3
-        ' 
-        Button3.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Button3.Location = New Point(699, 328)
-        Button3.Name = "Button3"
-        Button3.Size = New Size(94, 29)
-        Button3.TabIndex = 4
-        Button3.Text = "Total Net Profit"
-        Button3.UseVisualStyleBackColor = True
-        ' 
-        ' Button4
-        ' 
-        Button4.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Button4.Location = New Point(699, 390)
-        Button4.Name = "Button4"
-        Button4.Size = New Size(94, 29)
-        Button4.TabIndex = 5
-        Button4.Text = "Exit"
-        Button4.UseVisualStyleBackColor = True
-        ' 
-        ' Label1
-        ' 
-        Label1.AutoSize = True
-        Label1.ForeColor = SystemColors.ControlText
-        Label1.Location = New Point(56, 27)
-        Label1.Name = "Label1"
-        Label1.Size = New Size(103, 20)
-        Label1.TabIndex = 0
-        Label1.Text = "Part Identifier"
-        ' 
-        ' Label2
-        ' 
-        Label2.AutoSize = True
-        Label2.ForeColor = SystemColors.Desktop
-        Label2.Location = New Point(56, 65)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(119, 20)
-        Label2.TabIndex = 1
-        Label2.Text = "Part Description"
-        ' 
-        ' Label3
-        ' 
-        Label3.AutoSize = True
-        Label3.ForeColor = SystemColors.ControlText
-        Label3.Location = New Point(56, 54)
-        Label3.Name = "Label3"
-        Label3.Size = New Size(99, 23)
-        Label3.TabIndex = 1
-        Label3.Text = "Retail Price:"
-        ' 
-        ' Label4
-        ' 
-        Label4.AutoSize = True
-        Label4.ForeColor = SystemColors.ControlText
-        Label4.Location = New Point(56, 96)
-        Label4.Name = "Label4"
-        Label4.Size = New Size(131, 23)
-        Label4.TabIndex = 2
-        Label4.Text = "Wholesale Cost:"
-        ' 
-        ' Label5
-        ' 
-        Label5.AutoSize = True
-        Label5.ForeColor = SystemColors.Desktop
-        Label5.Location = New Point(56, 137)
-        Label5.Name = "Label5"
-        Label5.Size = New Size(81, 23)
-        Label5.TabIndex = 3
-        Label5.Text = "Quantity:"
-        ' 
-        ' Label6
-        ' 
-        Label6.AutoSize = True
-        Label6.ForeColor = SystemColors.ControlText
-        Label6.Location = New Point(56, 175)
-        Label6.Name = "Label6"
-        Label6.Size = New Size(103, 23)
-        Label6.TabIndex = 4
-        Label6.Text = "Gross Profit:"
-        ' 
-        ' Label7
-        ' 
-        Label7.AutoSize = True
-        Label7.ForeColor = SystemColors.ControlText
-        Label7.Location = New Point(56, 219)
-        Label7.Name = "Label7"
-        Label7.Size = New Size(140, 23)
-        Label7.TabIndex = 5
-        Label7.Text = "Storage Charges:"
-        ' 
-        ' Label8
-        ' 
-        Label8.AutoSize = True
-        Label8.ForeColor = SystemColors.ControlText
-        Label8.Location = New Point(56, 266)
-        Label8.Name = "Label8"
-        Label8.Size = New Size(89, 23)
-        Label8.TabIndex = 6
-        Label8.Text = "Net Profit:"
-        ' 
-        ' TextBox1
-        ' 
-        TextBox1.Location = New Point(240, 20)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(400, 27)
-        TextBox1.TabIndex = 2
-        TextBox1.Text = "X456YVK98"
-        ' 
-        ' TextBox2
-        ' 
-        TextBox2.Location = New Point(240, 65)
-        TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(400, 27)
-        TextBox2.TabIndex = 6
-        TextBox2.Text = "Radiator, Ford Escort"
         ' 
         ' TextBox3
         ' 
@@ -276,6 +176,106 @@ Partial Class Form1
         TextBox8.TabIndex = 12
         TextBox8.TabStop = False
         ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.ForeColor = SystemColors.ControlText
+        Label8.Location = New Point(56, 266)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(89, 23)
+        Label8.TabIndex = 6
+        Label8.Text = "Net Profit:"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.ForeColor = SystemColors.ControlText
+        Label7.Location = New Point(56, 219)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(140, 23)
+        Label7.TabIndex = 5
+        Label7.Text = "Storage Charges:"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.ForeColor = SystemColors.ControlText
+        Label6.Location = New Point(56, 175)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(103, 23)
+        Label6.TabIndex = 4
+        Label6.Text = "Gross Profit:"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.ForeColor = SystemColors.Desktop
+        Label5.Location = New Point(56, 137)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(81, 23)
+        Label5.TabIndex = 3
+        Label5.Text = "Quantity:"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.ForeColor = SystemColors.ControlText
+        Label4.Location = New Point(56, 96)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(131, 23)
+        Label4.TabIndex = 2
+        Label4.Text = "Wholesale Cost:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.ForeColor = SystemColors.ControlText
+        Label3.Location = New Point(56, 54)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(99, 23)
+        Label3.TabIndex = 1
+        Label3.Text = "Retail Price:"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(699, 207)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 2
+        Button1.Text = "Compute"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(699, 265)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 3
+        Button2.Text = "Reset Form"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button3.Location = New Point(699, 328)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 4
+        Button3.Text = "Total Net Profit"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button4.Location = New Point(699, 390)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 29)
+        Button4.TabIndex = 5
+        Button4.Text = "Exit"
+        Button4.UseVisualStyleBackColor = True
+        ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
@@ -289,7 +289,7 @@ Partial Class Form1
         Controls.Add(GroupBox2)
         Controls.Add(GroupBox1)
         Name = "Form1"
-        Text = "Form1"
+        Text = " "
         GroupBox1.ResumeLayout(False)
         GroupBox1.PerformLayout()
         GroupBox2.ResumeLayout(False)
--- Form1.vb
+++ Form1.vb
@@ -2,4 +2,25 @@
     Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
 
     End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        TextBox5.Clear()
+        TextBox6.Clear()
+        TextBox7.Clear()
+        TextBox8.Clear()
+
+
+        'Enable/disable buttons
+        Button1.Enabled = True
+        Button2.Enabled = False
+
+    End Sub
 End Class
